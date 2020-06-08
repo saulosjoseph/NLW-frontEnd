d73447bcf44fb3624b08c34b1e57e12ac3b8028f
Saulo de Sousa Joseph <saulos.joseph@gmail.com>
2020-06-08 19:23:39 -0300

    feat(leaflet): add

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -62,22 +63,30 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+          
+          <Map center={[-8.0917736, -34.8822944]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-8.0917736, -34.8822944]}/>
+          </Map>
 
-           <div className="field-group">
-             <div className="field">
-               <label htmlFor="uf">Estado (UF)</label>
-               <select name="uf" id="uf">
-                 <option value="0">Selectione uma UF</option>
-               </select>
-             </div>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selectione uma UF</option>
+              </select>
+            </div>
 
-             <div className="field">
-               <label htmlFor="city">Cidade</label>
-               <select name="city" id="city">
-                 <option value="0">Selectione uma Cidade</option>
-               </select>
-             </div>
-           </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selectione uma Cidade</option>
+              </select>
+            </div>
+          </div>
 
         </fieldset>
 
